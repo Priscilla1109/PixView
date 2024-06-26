616eb45f708b146d91bc6d69360ee580322e0f45
priscilla.fonseca <priscilla.fonseca@bv.com.br>
2024-06-26 17:50:03 -0300

    Adicionando método POST

--- PixView/src/main/java/com/demo/PixView/controller/UserController.java
+++ PixView/src/main/java/com/demo/PixView/controller/UserController.java
@@ -2,7 +2,7 @@ package com.demo.PixView.controller;
 
 import com.demo.PixView.model.User;
 import com.demo.PixView.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,14 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("APIs/PixView/users")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping
     public ResponseEntity<User> createNewUser(@RequestBody User user) {
--- PixView/src/main/java/com/demo/PixView/repository/JdbiUserRepository.java
+++ PixView/src/main/java/com/demo/PixView/repository/JdbiUserRepository.java
@@ -5,10 +5,10 @@ import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
-import org.springframework.stereotype.Repository;
 import org.jdbi.v3.sqlobject.locator.UseClasspathSqlLocator;
 
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
 
 @Repository
@@ -17,12 +17,12 @@ public interface JdbiUserRepository {
     @SqlUpdate
     void insert(@BindBean User user);
 
-    @SqlUpdate
-    void update(@BindBean User user);
-
-    @SqlQuery
-    Optional<User> getUserById(@Bind("id") Long id);
-
-    @SqlQuery
-    void deleteUser(@Bind("id") Long id);
+//    @SqlUpdate
+//    void update(@BindBean User user);
+//
+//    @SqlQuery
+//    Optional<User> getUserById(@Bind("id") Long id);
+//
+//    @SqlQuery
+//    void deleteUser(@Bind("id") Long id);
 }
